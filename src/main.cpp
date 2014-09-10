63bb4d756d6136afe03063b282653cc0b72882c5
Matt Richardson <mrichardson23@gmail.com>
2014-09-10 02:08:53 +0000

    committing progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include <wiringPi.h>
 
 //========================================================================
 int main( ){
 
-	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(1920,10800, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,7 +1,22 @@
 #include "ofApp.h"
+#include <wiringPi.h>
+#define WHEEL_SENSOR_PIN 7
+#define TOTAL_FRAMES 3
+
+ofImage frame[TOTAL_FRAMES];
+int currentFrame = 0;
+
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	for (int i = 0; i++; i < TOTAL_FRAMES) {
+		string filename = "Frame" + ofToString(i) + ".png";
+		frame[i].loadImage(filename);
+	}
+
+	wiringPiSetup();
+	pinMode(WHEEL_SENSOR_PIN, INPUT);
+	wiringPiISR (WHEEL_SENSOR_PIN, INT_EDGE_FALLING, &incrementFrame);
 
 }
 
@@ -13,6 +28,9 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 
+	if (digitalRead(WHEEL_SENSOR_PIN) == HIGH) {
+		frame[currentFrame].draw(0,0);
+	}
 }
 
 //--------------------------------------------------------------
@@ -56,6 +74,16 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
+}
+
+void incrementFrame () {
+	if (currentFrame < TOTAL_FRAMES - 1) {
+		currentFrame++;
+	}
+	else {
+		currentFrame = 0;
+	}
+	ofLog(OF_LOG_NOTICE, "currentFrame: %d", currentFrame);
 }
